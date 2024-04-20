6f164fd9ebfeefdfa69c945619a928505f243015
TanyaBabinets <tanya060616@gmail.com>
2024-04-20 20:40:33 +0300

    add p

--- src/App.js
+++ src/App.js
@@ -10,32 +10,32 @@ import PizzaList from './components/pizzas/pizza-list';
 import pizzas from './components/pizzas/pizzas.js';
 
 function App() {
- const [btnText, setBtnText] = useState("ДОБАВИТЬ ТОВАР");
+  const [btnText, setBtnText] = useState("ДОБАВИТЬ ТОВАР");
   const [modalActive, setModalActive] = useState(false);
   const [pList, setPList] = useState(pizzas);
   const addPizza = (h, price, image, description) => {
     console.log("Добавлена пицца:", h, price, image, description);
-      const newPizza = {
-          id: nanoid(),
-          h,
-          price,
-          image,
-          description
-      };
-      setPList([...pList, newPizza]);
+    const newPizza = {
+      id: nanoid(),
+      h,
+      price,
+      image,
+      description
+    };
+    setPList([...pList, newPizza]);
   };
- 
+
   useEffect(() => {
 
-    setBtnText(modalActive? "ЗАКРЫТЬ":"ДОБАВИТЬ ТОВАР")
+    setBtnText(modalActive ? "ЗАКРЫТЬ" : "ДОБАВИТЬ ТОВАР")
 
 
   }, [modalActive]);
   return (
-    
+
     <div>
       <h1 className="title">PIZZA-STORE</h1>
-      <button className='okno' onClick={() => setModalActive(!modalActive)}>{btnText }</button>
+      <button className='okno' onClick={() => setModalActive(!modalActive)}>{btnText}</button>
       <PizzaList pizzas={pList} />
       <Modal addPizza={addPizza} active={modalActive}>
         {/* <AddButton buttonAdd={addPizza} /> */}
